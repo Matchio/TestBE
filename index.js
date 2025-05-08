29e1290f17c154be86b9a1fdd72c061c2d39e4a9
Matchio <oaktana07@gmail.com>
2025-05-08 14:00:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(express.json());
 
